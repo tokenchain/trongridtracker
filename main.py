51bf64a8d3f44443bcbd6202009d5efc5098a2ec
heskdx <jobhesk@gmail.com>
2023-04-01 14:42:47 +0800

    ♻️  3 files changed, 21 insertions(+), 2 deletions(-) fix issue 1

--- main.py
+++ main.py
@@ -2,17 +2,15 @@
 # coding: utf-8
 from lib import USDTApp, CustomToken, Analysis
 
+scan_on_address = "...."
+
+
 def line_one_line():
-    # r = CustomToken().CheckTokenHolderAddress("TVddDhyFbFnq75S93Jydn4ueVD1B2zKoei", 6)
-    # addresspool="TVddDhyFbFnq75S93Jydn4ueVD1B2zKoei"
-    # addresspool="TDEz41eiEBE8LSjB2UvGNt2hkqmafhKX4Y"
-    scan_on_address = "TS2zT9XRqpD6ZU7GGGPBZMWhJrfFdr2KVM"
     USDTApp().CollectionTransactionFromTronForUSDT(scan_on_address)
     Analysis().start(scan_on_address)
 
 
 def only_read():
-    scan_on_address = "TS2zT9XRqpD6ZU7GGGPBZMWhJrfFdr2KVM"
     Analysis().start(scan_on_address)
 
 
